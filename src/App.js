e52d4a7e633997c5ed45ec080d070b1b10e9e832
christianguerra1 <guerra_hdez@hotmail.com>
2021-01-14 16:25:32 -0700

    Sign Up revision

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import {Login} from './Components/Login';
-import { SignIn } from './Components/SingIn';
-import { Container,Row, Col,Card, Form,Button } from 'react-bootstrap';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Login } from "./Components/Login";
+import { SignUp } from "./Components/SignUp";
+import { Container } from "react-bootstrap";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <Container 
+    <Container
       className="d-flex align-items-center justify-content-center"
-      style={{minHeight: "100vh"}}>
-      <Login />
-      {/* <SignIn /> */}
-      
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      {/* <Login /> */}
+      <SignUp />
     </Container>
   );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react'
-import { Container,Row, Col,Card, Form,Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Card, Form, Button } from "react-bootstrap";
 export class Login extends Component {
-    render(){
-        return(
-            <Card style={{ width: '18rem' ,backgroundColor: '#264d78',color:'white'}} className="mx-auto my-auto">
-                <Card.Header>Login</Card.Header>
-                <Card.Body>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                    </Form.Group>
+  render() {
+    return (
+      <Card
+        style={{ width: "18rem", backgroundColor: "#264d78", color: "white" }}
+        className="mx-auto my-auto"
+      >
+        <Card.Header>Login</Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button className="mx-auto" variant="primary" type="submit">
-                    Submit
-                    </Button>
-                </Form>
-                </Card.Body>
-            </Card>
-        )
-    }
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button className="mx-auto" variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
