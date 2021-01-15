985051b67b834b1d1221caf6edada333fa037154
eliuabdiel <2018030333@upsin.edu.mx>
2021-01-14 17:13:22 -0700

    adding routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import { Login } from "./Components/Login";
 import { SignUp } from "./Components/SignUp";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{
-        minHeight: "100vh",
-      }}
-    >
-      {/* <Login /> */}
-      <SignUp />
-    </Container>
+    <Router>
+        <Link to="/login">
+          <Button>
+            Login
+          </Button>
+        </Link> 
+        <Link to="singup">
+          <Button >
+            SignUp
+          </Button>
+        </Link> 
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{
+          minHeight: "100vh",
+        }}
+        >
+        
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/singup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
+
+    
   );
 }
 
