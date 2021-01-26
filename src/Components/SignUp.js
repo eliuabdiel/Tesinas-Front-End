39185719ec587e4299a181e57faddd34a85a84f6
eliuabdiel <2018030333@upsin.edu.mx>
2021-01-26 02:28:55 -0700

    Appliyied pasten style to the login and validation for the passwords, stlye is missing for validation

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,21 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import { Card, Form, Button } from "react-bootstrap";
 import logo from "../img/logo_oficial (1).png";
-import "../App.css";
+import "./SignUp.css";
 
 import {
   Link
 } from "react-router-dom";
 
 export function SignUp(){
-    let passwordsMatch = false
     const [passwords, setPasswords] = useState({
       password: '',
       passwordConfirmation: ''
     });
-    // useEffect(() => {
-    //   _passwordMatches()
-    // }, [passwords]);
+    
     function _handlePasswordChange(e) {
       setPasswords({
         password: e.target.value,
@@ -28,63 +25,42 @@ export function SignUp(){
         password  : passwords.password
       })
     }
-    // function _passwordMatches() {
-    //   if(passwords.passwordConfirmation.length !== 0 ){
-    //     if(passwords.passwordConfirmation !== passwords.password){
-    //       passwordsMatch = false
-          
-    //     }else{
-    //       passwordsMatch = true
-          
-    //     }
-    //   }
-    // }
+   
     return (
       <Card className="cardContainer">
         <Card.Body>
           <div className="logo">
-<<<<<<< HEAD
             <img src={logo} alt="UPSIN" width="120px"></img>
-=======
-            <img src={logo} alt="UPSIN" className="upsin"></img>
->>>>>>> 3be10688be0af4926e45230a416b6057cdec4cdd
           </div>
 
           <Form>
             <Form.Group>
-              <Form.Control type="text" placeholder="Usuario" />
+              <Form.Control
+                className="formInput"
+                type="text"
+                placeholder="Usuario"
+              />
             </Form.Group>
 
             <Form.Group>
               <Form.Control
-<<<<<<< HEAD
                 className="formInput"
                 type="email"
                 placeholder="Correo electronico"
-=======
-                type="password"
-                placeholder="Contraseña"
-                aria-describedby="pass"
->>>>>>> 3be10688be0af4926e45230a416b6057cdec4cdd
               />
             </Form.Group>
 
             <Form.Group>
               <Form.Control
+                className="formInput"
                 type="password"
-<<<<<<< HEAD
                 placeholder="Contraseña"
                 aria-describedby="pass"
                 onChange={_handlePasswordChange}
-=======
-                placeholder="Confirmar contraseña"
-                aria-describedby="pass"
->>>>>>> 3be10688be0af4926e45230a416b6057cdec4cdd
               />
             </Form.Group>
 
             <Form.Group>
-<<<<<<< HEAD
               <Form.Control
                 className="formInput"
                 type="password"
@@ -92,9 +68,6 @@ export function SignUp(){
                 aria-describedby="confirmPass"
                 onChange={_handlePasswordConfirmationChange}
               />
-=======
-              <Form.Control type="email" placeholder="Correo electronico" />
->>>>>>> 3be10688be0af4926e45230a416b6057cdec4cdd
             </Form.Group>
             {passwords.passwordConfirmation.length !== 0 
               ? passwords.passwordConfirmation !== passwords.password
@@ -103,13 +76,8 @@ export function SignUp(){
               : null
             }
             <div className="btnContainer">
-<<<<<<< HEAD
               <Button className="sendButton" variant="primary" type="submit">
                 Registrarse
-=======
-              <Button className="sendBtn" variant="primary" type="submit">
-                Enviar
->>>>>>> 3be10688be0af4926e45230a416b6057cdec4cdd
               </Button>
             </div>
             
