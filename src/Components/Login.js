4c4827b1b05bda91c3586887f87350750510f34b
christianguerra1 <guerra_hdez@hotmail.com>
2021-01-26 21:56:42 -0700

    Indentation and CSS fix

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,58 +1,52 @@
 import { Card, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../img/logo_oficial (1).png";
-import "./SignUp.css";
-import {
-  Link
-} from "react-router-dom";
+import "./styles.css";
 
 export function Login() {
+  return (
+    <Card className="cardContainer">
+      <Card.Body>
+        <div className="logo">
+          <img src={logo} alt="UPSIN" width="120px"></img>
+        </div>
 
-    return (
-      <Card className="cardContainer">
-        <Card.Body>
-          <div className="logo">
-            <img src={logo} alt="UPSIN" width="120px"></img>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              className="formInput"
+              type="text"
+              placeholder="Usuario"
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              className="formInput"
+              type="password"
+              placeholder="Contraseña"
+              aria-describedby="pass"
+            />
+          </Form.Group>
+
+          <div className="btnContainer">
+            <Button className="sendButton" variant="primary" type="submit">
+              Iniciar Sesión
+            </Button>
           </div>
+        </Form>
 
-          <Form>
-            <Form.Group>
-              <Form.Control
-                className="formInput"
-                type="text"
-                placeholder="Usuario"
-              />
-            </Form.Group>
-
-            <Form.Group>
-              <Form.Control
-                className="formInput"
-                type="password"
-                placeholder="Contraseña"
-                aria-describedby="pass"
-              />
-            </Form.Group>
-            
-            <div className="btnContainer">
-              <Button className="sendButton" variant="primary" type="submit">
-                Iniciar Sesión
-              </Button>
-            </div>
-
-          </Form>
-
-          <div className="txt centered">
-            <Link to="/signup">
-              <label className="mb-4">
-                Crear una cuenta.
-              </label>
-            </Link>
-            <label className="mb-0">Universidad Politécnica de Sinaloa</label>
-            <br></br>
-            <label className="mb-0">
-              <small>©2021 Todos los Derechos Reservados</small>
-            </label>
-          </div>
-        </Card.Body>
-      </Card>
-    );
+        <div className="txt centered">
+          <Link to="/signup">
+            <label className="mb-4">Crear una cuenta</label>
+          </Link>
+          <label className="mb-0">Universidad Politécnica de Sinaloa</label>
+          <br></br>
+          <label className="mb-0">
+            <small>©2021 Todos los Derechos Reservados</small>
+          </label>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,101 +1,97 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Card, Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../img/logo_oficial (1).png";
-import "./SignUp.css";
+import "./styles.css";
 
-import {
-  Link
-} from "react-router-dom";
+export function SignUp() {
+  const [passwords, setPasswords] = useState({
+    password: "",
+    passwordConfirmation: "",
+  });
 
-export function SignUp(){
-    const [passwords, setPasswords] = useState({
-      password: '',
-      passwordConfirmation: ''
+  function _handlePasswordChange(e) {
+    setPasswords({
+      password: e.target.value,
+      passwordConfirmation: passwords.passwordConfirmation,
     });
-    
-    function _handlePasswordChange(e) {
-      setPasswords({
-        password: e.target.value,
-        passwordConfirmation: passwords.passwordConfirmation
-      })
-    }
-    function _handlePasswordConfirmationChange(e){
-      setPasswords({
-        passwordConfirmation: e.target.value,
-        password  : passwords.password
-      })
-    }
-   
-    return (
-      <Card className="cardContainer">
-        <Card.Body>
-          <div className="logo">
-            <img src={logo} alt="UPSIN" width="120px"></img>
-          </div>
+  }
+  function _handlePasswordConfirmationChange(e) {
+    setPasswords({
+      passwordConfirmation: e.target.value,
+      password: passwords.password,
+    });
+  }
 
-          <Form>
-            <Form.Group>
-              <Form.Control
-                className="formInput"
-                type="text"
-                placeholder="Usuario"
-              />
-            </Form.Group>
+  return (
+    <Card className="cardContainer">
+      <Card.Body>
+        <div className="logo">
+          <img src={logo} alt="UPSIN" width="120px"></img>
+        </div>
 
-            <Form.Group>
-              <Form.Control
-                className="formInput"
-                type="email"
-                placeholder="Correo electronico"
-              />
-            </Form.Group>
+        <Form>
+          <Form.Group>
+            <Form.Control
+              className="formInput"
+              type="text"
+              placeholder="Usuario"
+            />
+          </Form.Group>
 
-            <Form.Group>
-              <Form.Control
-                className="formInput"
-                type="password"
-                placeholder="Contraseña"
-                aria-describedby="pass"
-                onChange={_handlePasswordChange}
-              />
-            </Form.Group>
+          <Form.Group>
+            <Form.Control
+              className="formInput"
+              type="email"
+              placeholder="Correo electronico"
+            />
+          </Form.Group>
 
-            <Form.Group>
-              <Form.Control
-                className="formInput"
-                type="password"
-                placeholder="Confirmar contraseña"
-                aria-describedby="confirmPass"
-                onChange={_handlePasswordConfirmationChange}
-              />
-            </Form.Group>
-            {passwords.passwordConfirmation.length !== 0 
-              ? passwords.passwordConfirmation !== passwords.password
-                ? <label className="text-danger">Las no contraseñas coinciden</label>
-                : <label className="text-success">Las contraseñas coinciden</label>
-              : null
-            }
-            <div className="btnContainer">
-              <Button className="sendButton" variant="primary" type="submit">
-                Registrarse
-              </Button>
-            </div>
-            
-          </Form>
+          <Form.Group>
+            <Form.Control
+              className="formInput"
+              type="password"
+              placeholder="Contraseña"
+              aria-describedby="pass"
+              onChange={_handlePasswordChange}
+            />
+          </Form.Group>
 
-          <div className="txt centered">
-            <Link to="/login">
-              <label className="mb-4">
-                Ya tengo una cuenta.
+          <Form.Group>
+            <Form.Control
+              className="formInput"
+              type="password"
+              placeholder="Confirmar contraseña"
+              aria-describedby="confirmPass"
+              onChange={_handlePasswordConfirmationChange}
+            />
+          </Form.Group>
+          {passwords.passwordConfirmation.length !== 0 ? (
+            passwords.passwordConfirmation !== passwords.password ? (
+              <label className="text-danger">
+                Las no contraseñas coinciden
               </label>
-            </Link>
-            <label className="mb-0">Universidad Politécnica de Sinaloa</label>
-            <label className="mb-0">
-              <small>©2021 Todos los Derechos Reservados</small>
-            </label>
+            ) : (
+              <label className="text-success">Las contraseñas coinciden</label>
+            )
+          ) : null}
+          <div className="btnContainer">
+            <Button className="sendButton" variant="primary" type="submit">
+              Registrarse
+            </Button>
           </div>
-        </Card.Body>
-      </Card>
-    );
-  
+        </Form>
+
+        <div className="txt centered">
+          <Link to="/login">
+            <label className="mb-4">Ya tengo una cuenta</label>
+          </Link>
+          <label className="mb-0">Universidad Politécnica de Sinaloa</label>
+          <label className="mb-0">
+            <small>©2021 Todos los Derechos Reservados</small>
+          </label>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
