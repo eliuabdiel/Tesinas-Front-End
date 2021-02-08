2c2de65313e70289e8266e201d0f98ea0ae37bd2
eliuabdiel <2018030333@upsin.edu.mx>
2021-02-07 23:43:58 -0700

    navbar and a profile page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,26 @@
-import Dashboard from "../Pages/Dashboard";
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import { Login } from "./Login";
 import { SignUp } from "./SignUp";
 import PrivateRoute from "./PrivateRoute";
 import NotFound from "../Pages/NotFound";
+import CustomNavbar from "./CustomNavbar";
+import { useAuth } from "../Contexts/AuthContext";
+import Home from "../Pages/Home";
+import Profile from "./Profile";
 
 
 
 function App() {
+  const { currentUser } = useAuth()
   return (
     <Router>
+        {currentUser && <CustomNavbar/>}
         <Switch>
           <PrivateRoute exact path='/'>
-            <Dashboard/> {/* tamemas este es el componente donde vamos a poner como la pagina de inicio es una ruta privada tienen q iniciar sesion para q puedan entrar creense un usuario */}
+            <Home/> {/* tamemas este es el componente donde vamos a poner como la pagina de inicio es una ruta privada tienen q iniciar sesion para q puedan entrar creense un usuario */}
+          </PrivateRoute>
+          <PrivateRoute path='/perfil'>
+            <Profile/> 
           </PrivateRoute>
           <Route path="/login">
             <Login/> {/*un tamemas pa q cambie los botones para que se vean mas bonitos, el boton de submit */}
